fcc6366e6b528f94ca9cb262d4831792e95b468e
akramjerbi91 <akram239911@gmail.com>
2022-03-13 21:45:23 +0100

    betty all files

---
+++ get_specifier.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+ * check_for_specifiers - function that selects the correct specifier
+ * @format: char pointer specifier
+ *
+ * Return: pointer to the correct func
+ */
+static int (*check_for_specifiers(const char *format))(va_list)
+{
+	register int i;
+	pr_t p[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"i", print_int},
+		{"d", print_int}
+	};
+
+	for (i = 0; p[i].type != NULL; i++)
+	{
+		if (*(p[i].type) == *format)
+		{
+			break;
+		}
+	}
+	return (p[i].f);
+}
---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stdarg.h>
+
+#define NULL 0
+
+
+int (*check_for_specifiers(const char *format))(va_list);
+int _print(const char * const format, ...);
+int _putchar(char c);
+int print_string(va_list args);
+int print_char(va_list args);
+int print_int(va_list args);
+void _puts(char *str);
+
+/**
+ * struct printer - Struct printer
+ * @type: The types
+ * @f: The function associated
+ */
+typedef struct printer
+{
+	char *type;
+	int (*f)(va_list args);
+} pr_t;
+#endif
