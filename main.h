6ee94c2cef4728019f61e596a71b493d8267a9f2
khawladaouay <daouaykhawla782@gmail.com>
2022-03-14 21:04:01 +0100

    printf

--- main.h
+++ main.h
@@ -2,7 +2,7 @@
 #define HEADER_FILE
 #include <stdarg.h>
 #include <stdio.h>
-int check_for_specifiers(const char format, va_list args);
+int (*check_for_specifiers(const char *format))(va_list);
 int _printf(const char * const format, ...);
 int _putchar(char c);
 int print_string(va_list args);
@@ -19,7 +19,7 @@ void _puts(char *str);
  */
 typedef struct printer
 {
-	char type;
+	char *type;
 	int (*f)(va_list);
 } pr_t;
 #endif
